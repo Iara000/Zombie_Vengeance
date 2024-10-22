5505a738ad296c646ef2c72a88359274fa15a47a
DaviSN <111313463+DaviSN2006@users.noreply.github.com>
2024-10-22 17:03:43 -0400

    Game 0.8

---
+++ Zombie Vengeance/Assets/Scripts/DestroyParticle.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+public class DestroyParticle : MonoBehaviour
+{
+    private float delayTimer = 1;
+	void Update ()
+    {
+        delayTimer -= Time.deltaTime;
+        if(delayTimer<= 0)
+        {
+            Destroy(this.gameObject);
+        }
+	}
+}
